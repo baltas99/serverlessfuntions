0840507491a6a68379d3a90a188e36678bc25448
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-18 11:49:01 +0200

    Update __init__.py

--- GetExchangeRates/__init__.py
+++ GetExchangeRates/__init__.py
@@ -3,7 +3,6 @@ import requests
 import os
 import json
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Get the base and target currencies from the query parameters
     base_currency = req.params.get('source', 'USD')  # Default to 'USD' if no base is provided
@@ -18,19 +17,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Use your API key from currencylayer.com
     api_key = os.environ.get('CURRENCY_LAYER_API_KEY')
     # Construct the API request URL with the base and target currencies
-    url = f"https://apilayer.net/api/live?access_key={api_key}&currencies={currencies}&source={base_currency}&format=1"
+    url = f"https://apilayer.net/api/live?access_key={api_key}&currencies={target_currencies}&source={base_currency}&format=1"
 
     response = requests.get(url)
     data = response.json()
 
-  if response.status_code == 200 and 'quotes' in data:
-    # The API returns keys like 'EURGBP', 'EURCAD', etc.
-    # We need to remove the source currency from the beginning of these keys
-    source_prefix = f"{base_currency}"  # e.g., "EUR"
-    rates = {currency[len(source_prefix):]: rate for currency, rate in data['quotes'].items()}
+    if response.status_code == 200 and 'quotes' in data:
+        # The API returns keys like 'EURGBP', 'EURCAD', etc.
+        # We need to remove the source currency from the beginning of these keys
+        source_prefix = f"{base_currency}"  # e.g., "EUR"
+        rates = {currency[len(source_prefix):]: rate for currency, rate in data['quotes'].items() if currency.startswith(source_prefix)}
 
-    return func.HttpResponse(json.dumps({'rates': rates}), mimetype="application/json")
-else:
-    # It's a good practice to return the error from the external API
-    error_message = data.get('error', {}).get('info', 'Failed to fetch the exchange rates')
-    return func.HttpResponse(error_message, status_code=response.status_code)
+        return func.HttpResponse(json.dumps({'rates': rates}), mimetype="application/json")
+    else:
+        # It's a good practice to return the error from the external API
+        error_message = data.get('error', {}).get('info', 'Failed to fetch the exchange rates')
+        return func.HttpResponse(error_message, status_code=response.status_code)
+
